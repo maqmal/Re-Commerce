a20ba53818c847c893f9d3b28279d0a734a9ddb5
aqmal pangestu <pangestuaqmal@gmail.com>
2022-02-07 23:46:53 +0700

    add nav

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shoppage/ShopPage';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route exact path='/shop' element={<ShopPage />} />
